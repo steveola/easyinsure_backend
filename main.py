d7a6abb170666b0efbf5e3942aad9d98e31faf0d
Ola Stephen <stivino1224@gmail.com>
2022-05-19 08:52:53 +0100

    First commitxxx23

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 #u_list  = list[schemas.User];
-# @app.get("/users/", response_model=schemas.User)
+# @app.get("/users/", response_model=u_list)
 # def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 #     users = crud.get_users(db, skip=skip, limit=limit)
 #     return users
@@ -83,7 +83,7 @@ def create_item_for_user(
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+# @app.get("/items/", response_model=list[schemas.Item])
+# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     items = crud.get_items(db, skip=skip, limit=limit)
+#     return items
